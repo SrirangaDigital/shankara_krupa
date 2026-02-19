7173f76c2247eba9cc6adbfdbae79d4b74935538
vinay <skanda312@gmail.com>
2026-02-19 12:32:19 +0530

    navbar changed

--- php/include_header.php
+++ php/include_header.php
@@ -29,8 +29,8 @@
 		<nav class="cd-main-nav">
 			<ul>
 				<li><a href="../index.html#home">ಮುಖಪುಟ</a></li>
-				<li><a href="../index.html#about">ಒಳನೋಟ</a></li>
-				<li><a href="../index.html#editors">ಸಂಪಾದಕರು</a></li>
+				<!-- <li><a href="../index.html#about">ಒಳನೋಟ</a></li> -->
+				<!-- <li><a href="../index.html#editors">ಸಂಪಾದಕರು</a></li> -->
 				<li><a href="volumes.php">ಸಂಗ್ರಹ</a> | <a href="volumes.php">ಸಂಪುಟಗಳು</a></li>
 				<li><a href="#">ಸಹಾಯ</a></li>
 			</ul>
